29448fa4bc7a1b2a5778ac416a278e03c8bfc772
bus710 <bus710@gmail.com>
2018-02-20 13:05:40 -0800

    A minor update

--- 10_vim_setup.sh
+++ 10_vim_setup.sh
@@ -1,7 +1,27 @@
 # Copying the vimrc file to my home directory.
 
+echo 
+echo 'Copied vimrc.'
+echo 
+
 cp vimrc_ubuntu.txt ~/.vimrc
+
+echo 
+echo 'Copied cobalt.vim in the colors directory.'
+echo 
+
 sudo cp cobalt.vim /usr/share/vim/vim74/colors/
+
+echo
+echo 'Got Vim-Plug'
+echo
+
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo
+echo 'Run :PluInstall in Vim'
+echo
 
 
 
