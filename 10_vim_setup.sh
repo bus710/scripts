c310a60a31f40ebfab5d759d1ba12713988dc564
bus710 <bus710@gmail.com>
2018-03-16 21:04:02 -0700

    A minor update

--- 10_vim_setup.sh
+++ 10_vim_setup.sh
@@ -21,7 +21,7 @@ curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo
-echo 'Run :PlugInstall in Vim'
+echo 'Run :PlugInstall in Vim (but YCM post process will fail...)'
 echo
 
 
