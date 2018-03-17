fa73dd4590052f102afac4d01b148149bd0062e1
bus710 <bus710@gmail.com>
2018-03-17 09:31:48 -0700

    A minor update

--- 10_vim_setup.sh
+++ 10_vim_setup.sh
@@ -16,7 +16,6 @@ echo
 echo 'Got Vim-Plug'
 echo
 
-mkdir $HOME/.vim/autoload
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
