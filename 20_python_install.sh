d4d5169bf8ff337b044781b15446e5dad42b7e1d
bus710 <bus710@gmail.com>
2018-06-07 21:59:32 -0700

    .

--- 20_python_install.sh
+++ 20_python_install.sh
@@ -1,9 +1,9 @@
 # Installing pip3 and libs for powerline-shell.
 
 sudo apt install -y python3-pip
-yes | sudo pip3 install powerline-status
-yes | sudo pip3 install powerline-gitstatus
-
 sudo apt install -y python3-pyqt5
 sudo apt install -y pyqt5-dev-*
 sudo apt install -y qttools5-dev-*
+
+yes | sudo pip3 install powerline-status
+yes | sudo pip3 install powerline-gitstatus
