2b54368abe962f97ef6aaee55318911f5c1b9109
bus710 <bus710@gmail.com>
2018-06-16 18:08:36 -0700

    .

--- 20_python_install.sh
+++ 20_python_install.sh
@@ -7,8 +7,8 @@ sudo apt install -y pyqt5-dev-*
 sudo apt install -y qttools5-dev-*
 
 pip3 install setuptools
-
-
+pip3 install wheel
+pip3 install pylint
 
 #yes | sudo pip3 install powerline-status
 #yes | sudo pip3 install powerline-gitstatus
