49fd03458d4e9c98458a72f418b4686adb1c16f1
bus710 <bus710@gmail.com>
2018-06-24 13:54:54 -0700

    A minor update

--- 11_golang_install.sh
+++ 11_golang_install.sh
@@ -4,5 +4,5 @@ echo "Check out below links"
 echo
 echo "https://golang.org/dl/"
 echo "https://github.com/bus710/golang-dev-env-setting"
-echo
+echo "https://www.jetbrains.com/go/download/#section=linux"
 echo
