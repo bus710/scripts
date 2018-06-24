1ffece58dccfab4b9c00e0dde6774ab78e0c4c9f
bus710 <bus710@gmail.com>
2018-06-24 13:13:50 -0700

    A minor update

--- 80_embedded_tools.sh
+++ 80_embedded_tools.sh
@@ -6,10 +6,10 @@ sudo apt install -y xsltproc # for BOM generating
 # BOM plugins can be found:
 # /usr/lib/kicad/plugins
 
+
+
 # chibios realted apps should be added here.
-
-echo "Download Eclipse in Download"
-
-wget http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/3/eclipse-cpp-neon-3-linux-gtk-x86_64.tar.gz $HOME/Download/eclipse-cpp-neon-3-linux-gtk-x86_64.tar.gz 
+# echo "Download Eclipse in Download"
+# wget http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/3/eclipse-cpp-neon-3-linux-gtk-x86_64.tar.gz $HOME/Download/eclipse-cpp-neon-3-linux-gtk-x86_64.tar.gz 
 
 
