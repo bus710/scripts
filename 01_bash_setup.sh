d7debbe7f9b6a6197451440e0a16c573ecbaad89
bus710 <bus710@gmail.com>
2018-06-24 17:36:54 -0700

    A minor update

--- 01_bash_setup.sh
+++ 01_bash_setup.sh
@@ -1,7 +1,14 @@
 # Copying the example bashrc file to the end of the default bashrc.
 cat ./bashrc_ubuntu.txt >> ~/.bashrc
 
+echo
 echo "Make a link to Desktop in case of using non-english distro."
 echo "ln -s XXXX Desktop"
 echo
+echo "Removing some default directories."
+echo
 
+rmdir ~/Documents
+rmdir ~/Music
+rmdir ~/Video
+rmdir ~/Pictures
---
+++ 29_dotnet_install.sh
@@ -0,0 +1,7 @@
+
+echo
+echo "Visit below link."
+echo
+echo "https://www.microsoft.com/net/learn/get-started/linux/ubuntu18-04"
+echo
+
