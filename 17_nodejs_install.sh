daa4c10f1e80d867b921a84209d2f40dfed27f30
bus710 <bus710@gmail.com>
2018-07-07 14:58:06 -0700

    A minor update

--- 17_nodejs_install.sh
+++ 17_nodejs_install.sh
@@ -1,4 +1,4 @@
 # Installing node.js v8.
 
-sudo curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y nodejs
