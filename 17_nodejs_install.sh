592db09280e95bc78764b8e03f4904ebab7ad244
bus710 <bus710@gmail.com>
2018-07-15 18:35:43 -0700

    A minor update

--- 17_nodejs_install.sh
+++ 17_nodejs_install.sh
@@ -2,3 +2,10 @@
 
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y nodejs
+
+echo
+echo "Some useful packages to be installed globally"
+echo
+echo "sudo npm i -g @vue/cli"
+echo "sudo npm i -g @vue/cli-init"
+echo
