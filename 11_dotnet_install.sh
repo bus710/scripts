fd3a5142705a8329b1710767234ba6bc32d4bc12
bus710 <bus710@gmail.com>
2018-07-26 21:39:32 -0700

    A minor update

---
+++ 11_dotnet_install.sh
@@ -0,0 +1,15 @@
+
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb ~/Desktop/
+sudo dpkg -i ~/Desktop/packages-microsoft-prod.deb
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-2.1
+
+echo
+echo "Done"
+echo
+echo "Visit below link."
+echo
+echo "https://www.microsoft.com/net/learn/get-started/linux/ubuntu18-04"
+echo
+
--- 30_dotnet_install.sh
+++
@@ -1,7 +0,0 @@
-
-echo
-echo "Visit below link."
-echo
-echo "https://www.microsoft.com/net/learn/get-started/linux/ubuntu18-04"
-echo
-
