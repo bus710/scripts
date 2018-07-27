288e227d8e799f5175a29977cb72b3f5ef3606c3
bus710 <bus710@gmail.com>
2018-07-26 22:27:41 -0700

    .

--- 11_dotnet_install.sh
+++ 11_dotnet_install.sh
@@ -1,9 +1,13 @@
 
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb ~/Desktop/
+
 sudo dpkg -i ~/Desktop/packages-microsoft-prod.deb
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-2.1
+
+dotnet new --install GtkSharp.Template.CSharp
+
 
 echo
 echo "Done"
