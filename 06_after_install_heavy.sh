582c83f24482208e39ade258a39262a189911c43
bus710 <bus710@gmail.com>
2018-11-19 16:51:55 -0800

    .

--- 06_after_install_heavy.sh
+++ 06_after_install_heavy.sh
@@ -4,13 +4,13 @@ echo
 echo "Install CPU Frequency Indicator"
 echo
 
-sudo apt install indicator-cpufreq
+sudo apt install indicator-cpufreq -y
 
 echo 
 echo "Install Simple Screen Recorder"
 echo
 
-sudo apt install simplescreenrecorder
+sudo apt install simplescreenrecorder -y
 
 
 echo 
