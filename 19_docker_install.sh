758d2b576edb477a6cc6fe0da5a257bde0268c8a
bus710 <bus710@gmail.com>
2019-06-05 10:07:15 -0700

    wip

--- 19_docker_install.sh
+++ 19_docker_install.sh
@@ -1,2 +1,10 @@
 curl -fsSL https://get.docker.com/ | sudo sh
 
+echo "docker build -t IMAGE_NAME DOCKERFILE_LOCATION"
+echo "docker images ls"
+echo "docker run -it --rm --name CONTAINER_NAME IMAGE_NAME"
+echo "docker ps -l"
+echo "docker container ls"
+echo "docker stop CONTAINER_NAME"
+echo "docker container prune"
+
