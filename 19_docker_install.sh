1a1481dd5287dabcee9b9fc66850adeee0b19896
bus710 <bus710@gmail.com>
2019-06-05 10:48:17 -0700

    A minor update

--- 19_docker_install.sh
+++ 19_docker_install.sh
@@ -3,6 +3,7 @@ curl -fsSL https://get.docker.com/ | sudo sh
 echo "docker build -t IMAGE_NAME DOCKERFILE_LOCATION"
 echo "docker images ls"
 echo "docker run -it --rm --name CONTAINER_NAME IMAGE_NAME"
+echo "docker run -p 8080:8080 -it --rm --name CONTAINER_NAME IMAGE_NAME"
 echo "docker ps -l"
 echo "docker container ls"
 echo "docker stop CONTAINER_NAME"
