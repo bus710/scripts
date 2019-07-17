dd376c9d9bcca00d606e17798c0f85cfc03289f9
bus710 <bus710@gmail.com>
2019-07-16 20:13:40 -0700

    A minor update

---
+++ 97_tlp.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update
+sudo apt-get install tlp tlp-rdw
+
+echo
+echo "in case of thinkpad"
+echo "sudo apt-get install tp-smapi-dkms acpi-call-dkms"
