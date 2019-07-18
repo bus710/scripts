50fb781c32b04051d2594691fae69b9da2bb4a63
bus710 <bus710@gmail.com>
2019-07-18 13:59:58 -0700

    A minor update

--- headless/04_docker_install_ubuntu.sh
+++ headless/04_docker_install_ubuntu.sh
@@ -9,7 +9,7 @@ sudo apt install \
 	git \
 	build-essential
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
