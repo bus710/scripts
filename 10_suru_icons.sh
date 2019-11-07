e401419a9215bb2acd1ac08a1a8b1b038110d352
bus710 <bus710@gmail.com>
2019-11-07 09:57:41 -0800

    A minor update

---
+++ 10_suru_icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install suru-icon-theme
+gsettings set org.gnome.desktop.interface icon-theme Suru
+
---
+++ 11_fcitx_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install fcitx-hangul
+im-config -n fcitx
+
+echo
+echo "reboot and run fcitx-config-gtk3"
+echo "also, install the korean support in settings"
+echo
