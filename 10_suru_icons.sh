761c207b3b2cd8279d0ccaa304449adb3a4104b4
bus710 <bus710@gmail.com>
2019-11-07 10:04:54 -0800

    A minor update

--- 10_suru_icons.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt install suru-icon-theme
-gsettings set org.gnome.desktop.interface icon-theme Suru
-
---
+++ 10_yaru_icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install yaru-theme-icon
+gsettings set org.gnome.desktop.interface icon-theme Yaru
+
