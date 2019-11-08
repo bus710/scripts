efe5b7ef805c81f265d0db764a3f0e98703152ca
bus710 <bus710@gmail.com>
2019-11-08 11:27:57 -0800

    A minor update

--- 03_heavy_packages.sh
+++ 03_heavy_packages.sh
@@ -13,21 +13,19 @@ echo
 sudo apt install simplescreenrecorder -y
 
 echo 
-echo "Install Chromium Browser"
+echo "Install Chrome Browser"
 echo 
 
-#sudo apt install -y chromium-browser
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update 
 sudo apt install -y google-chrome-stable
 
 echo 
-echo "Install Teak Tools"
+echo "Install Tweak Tools"
 echo 
 
 sudo apt-get install -y gnome-tweak-tool 
-#sudo apt-get install -y unity-tweak-tool
 
 echo
 echo "Install Gitkraken and its prerequisites"
@@ -36,19 +34,16 @@ echo
 sudo apt install -y gconf2 gconf-service libgtk2.0-0
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
-
-echo
-echo "Remove all deb files"
-echo
-
 rm *.deb
 
 echo
 echo "To install Vscode"
-echo "https://code.visualstudio.com/docs/?dv=linux64_deb"
-echo "https://code.visualstudio.com/docs/?dv=linux64_deb&build=insiders"
+echo "\t- https://code.visualstudio.com/docs/?dv=linux64_deb"
+echo "\t- https://code.visualstudio.com/docs/?dv=linux64_deb&build=insiders"
 echo
 echo "To install Slack"
-echo "https://slack.com/downloads/linux"
+echo "\t- https://slack.com/downloads/linux"
 echo
-
+echo "To install Android Studio"
+echo "\t- https://dl.google.com/dl/android/studio/ide-zips/3.5.2.0/android-studio-ide-191.5977832-linux.tar.gz"
+echo 
