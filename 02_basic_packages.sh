54fd48a71b23190a888be057983ad66a98a7f295
bus710 <bus710@gmail.com>
2019-11-26 07:11:34 -0800

    A minor update

--- 02_basic_packages.sh
+++ 02_basic_packages.sh
@@ -2,7 +2,7 @@
 sudo apt remove firefox thunderbird libreoffice*
 
 # Installing the basic utilities.
-sudo apt install -y usbtools \
+sudo apt install -y \
     net-tools \
     wireless-tools
 # sudo apt install -y libgnome-keyring0 #for gitkraken
