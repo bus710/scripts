0582e84c0da78343e54fe96d3588c72e049279bc
bus710 <bus710@gmail.com>
2020-02-29 18:34:37 -0800

    A minor update

--- 72_nodejs_install.sh
+++ 72_nodejs_install.sh
@@ -4,7 +4,8 @@ sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 
 echo
-echo "sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -"
-echo "sudo apt install -y nodejs"
+echo "What is done?"
+echo "1. sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -"
+echo "2. sudo apt install -y nodejs"
 echo
 
