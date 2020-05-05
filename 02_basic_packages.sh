b945ecce1a254e28fa91a16474b4ed5d20f88c02
bus710 <bus710@gmail.com>
2020-05-04 20:49:24 -0700

    A minor update

--- 02_basic_packages.sh
+++ 02_basic_packages.sh
@@ -1,14 +1,5 @@
 # Removing some heavy applications.
 sudo apt remove firefox thunderbird libreoffice*
 
-# Installing the basic utilities.
-sudo apt install -y \
-    net-tools \
-    wireless-tools \
-    ufw \
-    gufw \
-    inxi \
-    glmark2
-
 # sudo apt install -y libgnome-keyring0 #for gitkraken
 
