b572d120bee8a9e9d6ca8cde2d8e960c3404e27a
bus710 <bus710@gmail.com>
2020-05-25 14:27:33 -0700

    A minor update

--- 50_embedded_tools.sh
+++ 50_embedded_tools.sh
@@ -1,37 +1,17 @@
 echo
-echo "Install kicad, xsltproc"
+echo "Install kicad"
 echo
 
 sudo add-apt-repository --yes ppa:js-reynaud/kicad-5.1
 sudo apt update
 
-#sudo apt install -y kicad
-#sudo apt install -y kicad-libraries
-#sudo apt install -y kicad-symbols
-#sudo apt install -y kicad-footprints
+sudo apt install --install-recommends \
+    kicad kicad-demo kicad-local-en kicad-doc-en
 
-sudo apt install --install-recommends kicad kicad-demo kicad-local-en kicad-doc-en
-
-
-sudo apt install -y xsltproc # for BOM generating
+# sudo apt install -y xsltproc # for BOM generating
 
 # BOM plugins can be found:
 # /usr/share/kicad/plugins
-
-
-
-# chibios realted apps should be added here.
-# echo "Download Eclipse in Download"
-# wget http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/3/eclipse-cpp-neon-3-linux-gtk-x86_64.tar.gz $HOME/Download/eclipse-cpp-neon-3-linux-gtk-x86_64.tar.gz 
-
-echo
-echo "Install Picoscope"
-echo
-
-sudo bash -c 'echo "deb https://labs.picotech.com/debian/ picoscope main" >/etc/apt/sources.list.d/picoscope.list'
-wget -O - https://labs.picotech.com/debian/dists/picoscope/Release.gpg.key | sudo apt-key add -a
-sudo apt-get update
-sudo apt-get install picoscope
 
 echo
 echo "==== If KiCad Symbols are required ===="
