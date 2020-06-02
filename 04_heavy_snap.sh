e3d45595417b8ee624dd06fe264d0ec3e0af3128
bus710 <bus710@gmail.com>
2020-06-02 14:48:21 -0700

    A minor update

---
+++ 04_heavy_snap.sh
@@ -0,0 +1,17 @@
+echo
+echo "Install snapd and snap packages"
+echo
+
+sudo apt install snapd
+
+# Classic
+sudo snap install --classic goland
+sudo snap install --classic android-studio 
+sudo snap install --classic code 
+sudo snap install --classic code-insiders 
+sudo snap install --classic gitkraken 
+
+# Not classic
+sudo snap install mqtt-explorer
+
+echo
