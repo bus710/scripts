ab396eb7d94db0d1e2f4e1f8bb251573adf4ef9f
bus710 <bus710@gmail.com>
2020-06-04 11:16:31 -0700

    A minor update

--- 00_git_setup.sh
+++ 00_git_setup.sh
@@ -1,5 +1,5 @@
 git config remote.origin.url git@github.com:bus710/scripts.git
-git config --global user.email "bus710@gmail.com"
-git config --global user.name "bus710"
-git config --global push.default simple
+git config user.email "bus710@gmail.com"
+git config user.name "bus710"
+git config push.default simple
 
