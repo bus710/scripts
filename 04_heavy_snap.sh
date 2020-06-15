e642a4c87dc5f3b0e0ce04e3e165a78f7d4d891c
bus710 <bus710@gmail.com>
2020-06-15 14:48:32 -0700

    wip

--- 04_heavy_snap.sh
+++ 04_heavy_snap.sh
@@ -5,7 +5,7 @@ echo
 sudo apt install snapd
 
 # Classic
-sudo snap install --classic goland
+#sudo snap install --classic goland
 sudo snap install --classic android-studio 
 sudo snap install --classic code 
 sudo snap install --classic code-insiders 
@@ -22,20 +22,14 @@ cat ideavimrc >> ~/.ideavimrc
 
 echo
 echo "These are installed:"
-echo "- Goland"
 echo "- Android Studio"
 echo "- Vscode (+insider)"
 echo "- Mqtt explorer"
 echo
 
 echo
-echo "For JetBrains' IDEs:"
-echo "1. Mutual"
-echo "   Install Ideavim, Dracula, File Watcher"
-echo "2. Goland"
-echo "   gofmt: File > Settings > Tools > File Watcher"
-echo "   import unfolding: File > Editor > General > Code Folding"
-echo "3. Android Studio"
+echo "For Android Studio:"
+echo "   Install Ideavim, Dracula, File Watcher, Flutter"
 echo "   dartfmt: File > Settings > Languages > Flutter"
 echo "   SDK update: File > Settings > Appearance > System Settings > Android SDK"
 echo "   webdev issue: flutter pub pub cache repair"
